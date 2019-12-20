6e7e72aa5220955d718bc7dd44f2ca2df5ce9bc7
Garza0 <konovalovandrey7@gmail.com>
2019-12-20 02:14:30 +0200

    js part done

--- script.js
+++ script.js
@@ -297,3 +297,159 @@ function orbitalPeriod(arr) {
 }
 
 orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
+
+
+/**   JavaScript Algorithms and Data Structures Projects: */
+
+//=====Palindrome Checker 
+function palindrome(str) {
+  str = str.toLowerCase().match(/[a-z0-9]/g).join('')
+  let reverse = str.split('').reverse().join('')
+  return str == reverse
+}
+
+palindrome("My age is 0, 0 si ega ym.")
+
+
+
+
+//=====Telephone Number Validator
+function telephoneCheck(str) {
+  let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/
+  return regex.test(str)
+
+}
+
+telephoneCheck("555-555-5555");
+
+
+//=====Roman Numeral Converter
+
+function convertToRoman(num) {
+  const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+  let romanized = ''
+
+  for (let i = 0; i < decimal.length; i++) {
+    while (num >= decimal[i]) {
+      romanized += roman[i]
+      num -= decimal[i]
+    }
+  }
+  return romanized
+}
+
+convertToRoman(100)
+
+
+//=====Caesars Cipher
+function rot13(str) {
+  let input = str.split('')
+  let newStr = ''
+
+  for (let i = 0; i < input.length; i++) {
+    if (/\W/.test(input[i])) {
+      newStr += input[i]
+    } else if (str.charCodeAt(i) <= 77 && str.charCodeAt(i) > 64) {
+      newStr += String.fromCharCode(str.charCodeAt(i) + 13)
+    } else if (str.charCodeAt(i) > 77) {
+      newStr += String.fromCharCode(str.charCodeAt(i) - 13)
+
+    }
+  }
+
+  return newStr
+}
+
+// Change the inputs below to test
+rot13("SERR CVMMN!");
+
+
+
+
+//======Cash Register
+
+var denom = [
+  { name: "ONE HUNDRED", val: 100.0 },
+  { name: "TWENTY", val: 20.0 },
+  { name: "TEN", val: 10.0 },
+  { name: "FIVE", val: 5.0 },
+  { name: "ONE", val: 1.0 },
+  { name: "QUARTER", val: 0.25 },
+  { name: "DIME", val: 0.1 },
+  { name: "NICKEL", val: 0.05 },
+  { name: "PENNY", val: 0.01 }
+];
+
+function checkCashRegister(price, cash, cid) {
+  var output = { status: null, change: [] };
+  var change = cash - price;
+
+  // Transform CID array into drawer object
+  var register = cid.reduce(
+    function (acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+
+  // Handle exact change
+  if (register.total === change) {
+    output.status = "CLOSED";
+    output.change = cid;
+    return output;
+  }
+
+  // Handle obvious insufficient funds
+  if (register.total < change) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Loop through the denomination array
+  var change_arr = denom.reduce(function (acc, curr) {
+    var value = 0;
+    // While there is still money of this type in the drawer
+    // And while the denomination is larger than the change remaining
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+
+      // Round change to the nearest hundreth deals with precision errors
+      change = Math.round(change * 100) / 100;
+    }
+    // Add this denomination to the output only if any was used.
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc; // Return the current change_arr
+  }, []); // Initial value of empty array for reduce
+
+  // If there are no elements in change_arr or we have leftover change, return
+  // the string "Insufficient Funds"
+  if (change_arr.length < 1 || change > 0) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Here is your change, ma'am.
+  output.status = "OPEN";
+  output.change = change_arr;
+  return output;
+}
+
+// test here
+checkCashRegister(19.5, 20.0, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90.0],
+  ["FIVE", 55.0],
+  ["TEN", 20.0],
+  ["TWENTY", 60.0],
+  ["ONE HUNDRED", 100.0]
+]);
